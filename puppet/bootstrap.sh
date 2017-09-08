f1891d751d11988d222b06c29e1362d9d4815a5a
Denis Iskandarov <diskandarov@clips4sale.com>
2017-09-08 14:35:37 +0400

    puppet5 repo

--- puppet/bootstrap.sh
+++ puppet/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo 'Setting up "Puppet Collections" repository'
-yum install -y -q -e0 -d0 https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum install -y -q -e0 -d0 https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
 
 echo 'Installing Puppet Agent package'
 yum install -y -q -e0 -d0 puppet-agent
